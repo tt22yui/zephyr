28c555dc406131595b0e4291b4c4d693bc898444
tiantian <tt2yui@hotmailcom>
2026-09-05 14:32:28 +0800

    feat: 屏蔽原生右键菜单

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// 屏蔽原生右键菜单：应用级 UI，右键不应弹出浏览器/WebView 的默认菜单。
+window.addEventListener("contextmenu", (e) => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
